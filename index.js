baf4d4448866a8684edee23d0f6a062d69ef6c80
Kenneth Kim <kkm2059@gmail.com>
2021-11-05 11:11:46 -0400

    commit tree 186a96ef20a442700ebd639d420360a8fdde9d06

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+main.remove()
+
+//
+
+// const H1 = document.getElement("main");
+// main.innerHTML = 
+//     "<h1 id=`victory`>`victory`</h1>"
+
+const newHeader = document.getElementById("victory")
